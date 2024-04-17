cb9b5ae9eb8dfd5c0923631c1dfea17959b2b1fb
Venii24 <stefanieplankensteiner24@gmail.com>
2024-04-17 14:08:13 +0200

    implemented collision with wall push box finish game

--- libs/obj/Debug/net8.0/libs.AssemblyInfo.cs
+++ libs/obj/Debug/net8.0/libs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("libs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+150f0a7ade5436d682df236168f45ef0094977ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7bff4df6f2ece61cacd5a2b2c8ea762adf40a2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("libs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("libs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
